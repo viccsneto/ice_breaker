e8700085d42432b3989418ef2b0a79226f4d5958
Vicente Corrêa da Silva Neto <viccsneto@gmail.com>
2025-04-27 21:31:12 -0300

    Implement summary generation for a person using LangChain and OpenAI

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,47 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+SUMMARY_TEMPLATE = \
+"""	
+given the information {information} about a person, write:
+1. a short summary of the person
+2. two interesting facts about the person
+"""     
+
+def generate_summary(information: str) -> str:
+    summary_prompt = PromptTemplate(
+        input_variables=["information"],
+        template=SUMMARY_TEMPLATE
+    )
+
+    llm = ChatOpenAI(
+        model=os.environ.get("LLM_MODEL"),
+        temperature=0)
+    
+    summary_chain = summary_prompt | llm
+
+    response = summary_chain.invoke({
+        "information": information
+    })
+    
+    return response.content
 
 if __name__ == "__main__":
     load_dotenv()
-    print("Hello, LangChain!")
-    print("Using OPENAI_API_BASE as:", os.environ["OPENAI_API_BASE"])
+    information = """
+Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman known for his leadership of Tesla, SpaceX, and X (formerly Twitter). Since 2025, he has been a senior advisor to United States president Donald Trump and the de facto head of the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of March 2025, Forbes estimates his net worth to be US$345 billion. He was named Time magazine's Person of the Year in 2021.
+
+Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He graduated from the University of Pennsylvania in the U.S. before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became a U.S. citizen.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence research but later left; growing discontent with the organization's direction in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. In January 2025, he was appointed head of Trump's newly created DOGE. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
+
+Musk's political activities and views have made him a polarizing figure. He has been criticized for making unscientific and misleading statements, including COVID-19 misinformation and promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. Especially since the 2024 U.S. presidential election, Musk has been heavily involved in politics as a vocal supporter of Trump. Musk was the largest donor in the 2024 U.S. presidential election and is a supporter of global far-right figures, causes, and political parties. His role in the second Trump administration, particularly in regards to DOGE, has attracted public backlash.
+    """
+
+    response = generate_summary(information)
+    
+    print(response)
+
+
